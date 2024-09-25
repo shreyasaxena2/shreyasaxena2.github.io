6f6ba03f96444b93bbb211de06b7ac8734ce87a2
shreyasaxena2 <shreyasaxena@outlook.com>
2024-09-24 22:02:21 -0600

    decided on what to do

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -7,7 +7,7 @@
 
 
 
-let diam = 30
+
 
 
 function setup() {
@@ -16,19 +16,11 @@ function setup() {
 
 function draw() {
   background(220);
-  ball()
 
 }
 
 
 function ball() {
-  if (((mouseX <= windowWidth) && (mouseY === 0)) || ((mouseX === 0) && (mouseY <= windowHeight))) {
-      diam = diam + 2
-      circle(mouseX, mouseY, diam)
-  }
-  else {
-    circle(mouseX, mouseY, diam)    
-  }
 
   
 }
