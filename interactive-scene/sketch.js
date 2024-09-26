0ccee6ad860cd3d8622a30501e6667d1af863ea7
shreyasaxena2 <shreyasaxena@outlook.com>
2024-09-25 22:26:49 -0600

    added start screen

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -7,20 +7,38 @@
 
 
 let medha;
-
+let transition = false;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-
+  
 function draw() {
   startScreen();
-
 }
 
 
 function startScreen() {
   background("#A7C7E7"); // pastel blue background
-  
+  textSize(65)
+  text("PRESS THE SPACE BAR", (width/2), height/2);
+  textAlign(CENTER, CENTER);  
+  // if (keyIsPressed === true) {
+  //   if (key === "space" ) {
+  //     background("pink");
+  //   }
 }
+
+
+  
+ 
+
+
+// function trans() {
+//   while (transition === true) {
+//     rotateY(frameCount / 30);
+//     textSize(65);
+//     text("HAPPY BIRTHDAY MEDHA", (width/2), height/2);
+//     textAlign(CENTER, CENTER);
+//   }
