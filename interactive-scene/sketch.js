96d486b3cbd3d2c6a585047ccd995d96f2a23add
shreyasaxena2 <shreyasaxena@outlook.com>
2024-10-01 14:16:06 -0600

    added more text

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -11,9 +11,12 @@ let balloons;
 let hbds;
 let gifLoadImg;
 let gifCreateImg;
-let isPlaying = false
+let isPlaying = false;
 let fStartScreen;
+let cake;
 let fHbd;
+let fclickimg;
+let bgStartImg;
 
 
 
@@ -21,12 +24,13 @@ function preload() {
   balloons = loadImage("balloon.png");
   medha = loadImage("medmed.png");
   hbds = loadSound("hbds.mp3");
-  cake = loadImage("bdaycakeimg.png")
+  cake = loadImage("bdaycakeimg.png");
   gifLoadImg = loadImage("birthdaygif.gif");
     
-  fStartScreen = loadFont("startscreenfont.otf")
-  fHbd = loadFont("bdayfont.ttf")
-  fclickimg = loadFont("clickfont.ttf")
+  fStartScreen = loadFont("startscreenfont.otf");
+  fHbd = loadFont("bdayfont.ttf");
+  fclickimg = loadFont("clickfont.ttf");
+  bgStartImg = loadImage("startscreenimg.jpg");
 
 }
 
@@ -34,7 +38,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   startScreen();
-  windowResized();
+  //windowResized();
 }
 
 
@@ -46,22 +50,23 @@ function draw() {
 
 
 function startScreen() {
-  background("#A7C7E7"); // pastel blue background
+  background(bgStartImg); // pastel blue background
   textAlign(CENTER, CENTER);  
   textSize(65);
-  textFont(fStartScreen)
-  text("PRESS THE SPACE BAR", (width/2), height/2); 
+  textFont(fStartScreen);
+  text("PRESS THE SPACE BAR", width/2.2, height/4.5); 
 }
 
 
 
 function keyPressed() { 
   if (keyIsDown(32) ) {
-    background("pink");
+    background("#A7C7E7");
     textAlign(CENTER, CENTER);
     textSize(100);
     textFont(fHbd);
-    text("Happy Birthday Medha!!", (width/2.2), height/4.5);
+    text("Happy Birthday Medha!!", width/2.2, height/4.5);
+    text("Have the best day ever!!", width/1.25, height/1.5);
     clickableImage();
     collagePictures();
   } 
@@ -72,7 +77,7 @@ function keyPressed() {
 function clickableImage() {
   image(balloons, width - balloons.width * 4, balloons.height * 0.1 , balloons.width * 0.75, balloons.height * 0.75);
   textSize(30);
-  textFont(fclickimg)
+  textFont(fclickimg);
   text("Click the balloon for a surprise!", width - width * 0.89, height * 0.45);
   mouseClicked();
 } 
@@ -82,21 +87,21 @@ function clickableImage() {
 
 
 function mouseClicked() {
-    if (((mouseX <= balloons.width && mouseX >= width - balloons.width * 4) && (mouseY <= balloons.height && mouseY >= balloons.height * 0.1)) && isPlaying === false){
-      hbds.play();
-      image(cake, width - cake.width * 3.7, height - cake.height * 0.9, cake.width * 0.8, cake.height * 0.8)
+  if (mouseX <= balloons.width && mouseX >= width - balloons.width * 4 && (mouseY <= balloons.height && mouseY >= balloons.height * 0.1) && isPlaying === false){
+    hbds.play();
+    image(cake, width - cake.width * 3.7, height - cake.height * 0.9, cake.width * 0.8, cake.height * 0.8);
 
-      gifCreateImg = createImg("birthdaygif.gif");
-      gifCreateImg.position(width - gifLoadImg.width * 1.1,  gifLoadImg.height * 0.1);
+    gifCreateImg = createImg("birthdaygif.gif");
+    gifCreateImg.position(width - gifLoadImg.width * 1.1,  gifLoadImg.height * 0.1);
       
-      isPlaying = true
+    isPlaying = true;
 
   }
 
 }
 
 function collagePictures() {
-  image(medha, (width - medha.width * 2.3), (height - cake.height * 1)); 
+  image(medha, width - medha.width * 2.3, height - cake.height * 1); 
 }
 
 
