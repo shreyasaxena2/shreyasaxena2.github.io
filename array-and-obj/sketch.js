1d3edacee9852d13840063093ee83ef7700ddd66
shreyasaxena2 <shreyasaxena@outlook.com>
2024-10-16 14:15:38 -0600

    eslint errors fixed

--- array-and-obj/sketch.js
+++ array-and-obj/sketch.js
@@ -20,13 +20,13 @@ function draw() {
   strokeWeight(7);
 
   // VERTICAL LINES
-  for (var i = 0; i < 25; i++){ 
-    line(width/25*i, 0, width/25*i, height)
+  for (let i = 0; i < 25; i++){ 
+    line(width/25*i, 0, width/25*i, height);
   }
 
   // HORIZONTAL LINES
-  for (var i = 0; i < 25; i++){ 
-    line(0, height/25*i, width, height/25*i)
+  for (let i = 0; i < 25; i++){ 
+    line(0, height/25*i, width, height/25*i);
   }
 
 }
