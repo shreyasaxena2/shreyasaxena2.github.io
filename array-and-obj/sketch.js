1babf966fe25cf194485d90e41c7ba870d68d832
shreyasaxena2 <shreyasaxena@outlook.com>
2024-10-18 14:15:36 -0600

    created hail and changed the shape to ellipsies

--- array-and-obj/sketch.js
+++ array-and-obj/sketch.js
@@ -8,25 +8,41 @@
 // https://editor.p5js.org/arthurfincham/sketches/D-3Ty82rT source code
 
 
+let hailArray = [];
+let hail;
+
 function setup() {
-  createCanvas(750, 750);
+  createCanvas(windowWidth, windowHeight);
+  
+  for (let i = 0; i < 5; i++) {
+    createHail();
+  }
 }
 
 function draw() {
-  background(0);
+  background(165);
+  showHail();
 
-  // LINE STYLE
-  stroke('white');
-  strokeWeight(7);
+}
 
-  // VERTICAL LINES
-  for (let i = 0; i < 25; i++){ 
-    line(width/25*i, 0, width/25*i, height);
+
+function showHail() {
+  for (let hail of hailArray) {
+    noStroke();
+    fill(hail.r, hail.g, hail.b, hail.alpha);
+    circle(hail.x, hail.y, hail.radius * 2);
   }
+}
 
-  // HORIZONTAL LINES
-  for (let i = 0; i < 25; i++){ 
-    line(0, height/25*i, width, height/25*i);
-  }
-
+function createHail() {
+  let hail = {
+    x: random(0, width),
+    y: random(0, height),
+    radius: random(10, 20),
+    r: random(255),
+    g: random(255),
+    b: random(255),
+    alpha: random(255),
+  };
+  hailArray.push(hail);
 }
