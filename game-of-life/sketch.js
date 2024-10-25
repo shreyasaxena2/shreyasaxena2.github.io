e0d7d48c5932eef77d97df4e8a0ba86f59e570cc
shreyasaxena2 <shreyasaxena@outlook.com>
2024-10-25 13:59:22 -0600

    game of life demo

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -1,15 +1,191 @@
-// Game of Life
-// Shreya Saxena
-// 25th October, 2025
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Grid Demo
+// Dan Schellenberg
+// Oct 22, 2024
 
+// if hardcoding the grid, use something like this:
+// let grid = [[1, 0, 1, 0],
+//             [0, 0, 1, 1],
+//             [1, 1, 1, 0],
+//             [0, 1, 1, 0]];
+
+let grid;
+const GRID_SIZE = 40;
+let cellSize;
+let shouldToggleNeighbours = false;
+let autoPlayIsOn = false;
+let renderOnFrameMultiple = 5;
+let gosperGun;
+
+
+function preload() {
+  gosperGun = loadJSON("gosper.json");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
+  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+}
+
+function windowResized() {
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
 }
 
 function draw() {
   background(220);
+  if (autoPlayIsOn && frameCount % renderOnFrameMultiple === 0) {
+    grid = updateGrid();
+  }
+
+  displayGrid();
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  //toggle self
+  toggleCell(x, y);
+
+  //toggle neighbours
+  if (shouldToggleNeighbours) {
+    toggleCell(x + 1, y);
+    toggleCell(x - 1, y);
+    toggleCell(x, y + 1);
+    toggleCell(x, y - 1);
+  }
+}
+
+function toggleCell(x, y) {
+  //make sure the cell you're toggling is in the grid
+  if (x >= 0 && y >= 0 && x < GRID_SIZE && y < GRID_SIZE) {
+    if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+    else {
+      grid[y][x] = 1;
+    }
+  }
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  }
+  if (key === "e") {
+    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  }
+  if (key === "n") {
+    shouldToggleNeighbours = !shouldToggleNeighbours;
+  }
+  if (key === " ") {
+    grid = updateGrid();
+  }
+  if (key === "a") {
+    autoPlayIsOn = !autoPlayIsOn;
+    
+  }
+  if (key === "g") {
+    grid = gosperGun;
+  }
+}
+
+function updateGrid() {
+  //make a new array to hold the next turn
+  let nextTurn = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+
+  //look at every cell
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      //count it's neighbours
+      let neighbours = 0;
+
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //don't fall of the edge
+          if (y+i >= 0 && y+i < GRID_SIZE && x+j >= 0 && x+j < GRID_SIZE) {
+            neighbours += grid[y+i][x+j];
+          }
+        }
+      }
+
+      //don't count yourself
+      neighbours -= grid[y][x];
+
+      //apply the rules 
+      if (grid[y][x] === 0) {
+        //currently dead
+        if (neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 1) {
+        //currently alive
+        if (neighbours === 2 || neighbours === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+    }
+  }
+  return nextTurn;
+}
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === 1) {
+        fill("black");
+      }
+      else if (grid[y][x] === 0) {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      //choose either 0 or 1, each 50% of the time
+      if (random(100) < 50) {
+        newGrid[y].push(1);
+      }
+      else {
+        newGrid[y].push(0);
+      }
+    }
+  }
+  return newGrid;
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      newGrid[y].push(0);
+    }
+  }
+  return newGrid;
 }
