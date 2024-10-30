0fd1de9181d08bea15d0064e21a09b39596d0b3a
shreyasaxena2 <shreyasaxena@outlook.com>
2024-10-30 14:14:11 -0600

    part of the grid created

--- grid-assingment/sketch.js
+++ grid-assingment/sketch.js
@@ -1,15 +1,47 @@
-// Project Title
-// Your Name
+// Grid Based Game - pac man
+// Shreya Saxena 
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let player;
+const GRID_SIZE = 20;
+const CAN_MOVE = 1;
+const WALL = 0;
+
+
+function preload() {
+  player = loadImage("player-pac-man.png");
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+
 }
 
 function draw() {
   background(220);
+}
+
+
+function generateGrid(rows, columns) {
+  let theGrid = [];
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < columns; x++){
+      if (100 < 50) {
+        theGrid.push(CAN_MOVE);
+      }
+      else {
+        theGrid.push(WALL);
+      }
+    }
+  }
+
 }
