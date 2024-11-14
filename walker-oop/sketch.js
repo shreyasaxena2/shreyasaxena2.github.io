d57d54243d2941d1aa6dede2839015b22572175a
shreyasaxena2 <shreyasaxena@outlook.com>
2024-11-14 13:13:17 -0600

    fixed demo

--- walker-oop/sketch.js
+++ walker-oop/sketch.js
@@ -1,39 +1,54 @@
 // Walker OOP Demo
-// Shreya Saxena
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
 
 class Walker {
-  constructor(x, y) {
+  constructor(x, y, theColor) {
     this.x = x;
     this.y = y;
-    this.speed = 5;
+    this.speed = 8;
     this.radius = 3;
     this.color = theColor;
   }
 
   display() {
-    circle(this.x, this.y, this.radius * 2);
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y, this.radius*2);
   }
-  
-}
 
-
-move() {
-  let choice = ramf=
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+      //up
+      this.y -= this.speed;
+    }
+    else if (choice < 50) {
+      //down
+      this.y += this.speed;
+    }
+    else if (choice < 75) {
+      //left
+      this.x -= this.speed;
+    }
+    else {
+      //right
+      this.x += this.speed;
+    }
+  }
 }
 
 let luc;
-
+let michael;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let luc = new Walker(width/2, height/2);
+  luc = new Walker(width/2, height/2, "red");
+  michael = new Walker(300, 200, "blue");
 }
 
 function draw() {
-  background(220);
+  michael.move();
+  luc.move();
+
+  michael.display();
+  luc.display();
 }
